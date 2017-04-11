93837ffee1b3242d055a1f8bc2cc34d49da38865
s00144297 <s00144297@mail.itsligo.ie>
2017-04-11 13:07:19 +0100

    Fixed issue where the wrong image would sometimes display for a venue/ event/ band - Cathal

---
+++ ToDo/Controllers/BandFileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class BandFileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /File/
+        public ActionResult Index(int id)
+        {
+            //Band File
+            var BfileToRetrieve = db.BandFiles.Find(id);
+
+
+            if (BfileToRetrieve != null)
+            {
+                return File(BfileToRetrieve.BandContent, BfileToRetrieve.BandContentType);
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ToDo/Controllers/VenueFileController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class VenueFileController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        //
+        // GET: /File/
+        public ActionResult Index(int id)
+        {
+            //Venue File
+            var VfileToRetrieve = db.VenueFiles.Find(id);
+
+
+            if (VfileToRetrieve != null)
+            {
+                return File(VfileToRetrieve.VenueContent, VfileToRetrieve.VenueContentType);
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
